934dee3c49c049ea7e62ec5f6e3d466259a3499b
Tajwar <tajwaraashab@gmail.com>
2022-11-06 01:51:54 +0600

    cleaned up

--- rotate.py
+++ rotate.py
@@ -5,24 +5,25 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 class RotatePdf(Resource):
     def post(self):
+        # get the request body data
         input_data = request.get_json()
-        target_file = input_data['file_path']
-
-        output_path = 'rotated.pdf' 
-
-        output_file = open(output_path, 'wb')
-        pdf = PdfFileReader(target_file)
-        writer = PdfFileWriter()
-
-        # total number of pages
-        pages = pdf.getNumPages()
-
         # the page number to rotate
         page_number = int(input_data['page_number']) - 1
 
         # angle of rotation
         angle_of_rotation = int(input_data['angle_of_rotation'])
 
+        target_file = input_data['file_path']
+
+        output_path = f'/tmp/rotated_{target_file}' 
+
+        output_file = open(output_path, 'wb')
+        
+        pdf = PdfFileReader(target_file)
+        writer = PdfFileWriter()
+
+        # total number of pages
+        pages = pdf.getNumPages()
 
 
         for i in range(pages):
