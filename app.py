011430af845fc4e1b52037c9f9b74e07c682b64e
Tajwar <tajwaraashab@gmail.com>
2022-11-06 00:27:01 +0600

    rotate pdf

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 from flask import Flask
 from flask_restful import Resource, Api 
 
+from rotate import RotatePdf
 
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(RotatePdf, '/submit')
 
 if __name__ == '__main__':
     app.run(debug=True)
